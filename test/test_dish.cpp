fa1628e20c1ca02303162e26251af22a09a081f9
mmaneh <mmanemkhitaryan@gmail.com>
2025-06-21 19:01:45 +0400

    Remove test folder from repo and add to .gitignore

--- test/test_dish.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include "../dish.h"
-
-TEST(DishTest, BasicProperties) {
-    Dish d("Pizza", 12.5);
-    EXPECT_EQ(d.getName(), "Pizza");
-    EXPECT_DOUBLE_EQ(d.getPrice(), 12.5);
-}
